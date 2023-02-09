26d5901d2b389c8b18509f72e737b1eba6e9ba28
Hoang Ly <hoangly@Hoangs-MacBook-Pro.local>
2023-02-09 18:39:05 +0700

    feat: menu humburger for mobile

--- src/features/Header/Menu/index.tsx
+++ src/features/Header/Menu/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
+import useClickOutside from '@/hooks/useClickOutside';
 
 const Menu = () => {
   const router = useRouter();
   const paths = router.asPath.split('/');
+  const [isVisible, setVisible] = useState(false);
+
+  const {ref} = useClickOutside({
+    clickOutsideCallback: ()=> {setVisible(false)},
+    clickInsideCallback: ()=> {setVisible(true)}
+  });
 
   const menuItems = [
     {
@@ -17,23 +24,34 @@ const Menu = () => {
       href: 'road-map'
     },
   ];
-
+console.log(isVisible)
   return (
-    <div className={styles.menu}>
-      {
-        menuItems.map((menuItem, index)=>(
-          <a 
-          key={index} 
-          href={`/${menuItem.href}`}
-          className={classNames(
-            paths[1] === menuItem.href && styles.active
-          )}
-          >
-            {menuItem.label}
-          </a>
-        ))
-      }
-    </div>
+    <div className={styles.menuWrapper}>
+      <div className={styles.humburger} ref={ref}>
+        <img src="https://sisu.network/assets/hamburger.dd7ebf99.svg" alt="Menu" />
+      </div>
+      <div 
+      className={classNames(
+        styles.menu,
+        isVisible && styles.opened
+      )}
+      >
+        {
+          menuItems.map((menuItem, index) => (
+            <div key={index}>
+              < a
+                href={`/${menuItem.href}`}
+                className={classNames(
+                  paths[1] === menuItem.href && styles.active
+                )}
+              >
+                {menuItem.label}
+              </a>
+            </div>
+          ))
+        }
+      </div >
+    </div >
   );
 };
 
